1569c78cce7f7b170ae4fa6e1b035b12509cf32c
Mark Clifford <marklearninglanguage@gmail.com>
2022-03-22 08:51:29 -0700

    added A major to core

--- src/core.js
+++ src/core.js
@@ -1,3 +1,17 @@
 function brain(input) {
     return { name: 'vcrap', notes: ['A', 'B'] }
+    var AMaj = [
+        { letter: 'A', flavor: 'natural' },
+        { letter: 'B', flavor: 'natural' },
+        { letter: 'C', flavor: 'sharp' },
+        { letter: 'D', flavor: 'natural' },
+        { letter: 'E', flavor: 'natural' },
+        { letter: 'F', flavor: 'sharp' },
+        { letter: 'G', flavor: 'sharp' },
+        { letter: 'A', flavor: 'natural' },
+    ]
+
 }
+
+
+
