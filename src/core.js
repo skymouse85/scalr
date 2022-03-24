517e1def275315dbc46553ad1ac938860c453907
Mark Clifford <marklearninglanguage@gmail.com>
2022-03-24 16:08:23 -0700

    initial scale construct function

--- src/core.js
+++ src/core.js
@@ -15,3 +15,32 @@ function brain(input) {
 
 
 
+
+
+function scaleConstruct(sclObjArr) {
+    var result = "";
+    for (let i = 0; i < sclObjArr.length; i++) {
+        var noteObj = sclObjArr[i];
+        // for (var key in noteObj) {
+        //     result += noteObj.letter;
+        //     // result += noteObj.flavor;
+        //     result += ','
+        // }
+        result += noteObj.letter;
+        result += ' '
+        result += noteObj.flavor;
+        result += ' ';
+    }
+    return result;
+}
+
+console.log(scaleConstruct([
+    { letter: 'A', flavor: 'natural' },
+    { letter: 'B', flavor: 'natural' },
+    { letter: 'C', flavor: 'sharp' },
+    { letter: 'D', flavor: 'natural' },
+    { letter: 'E', flavor: 'natural' },
+    { letter: 'F', flavor: 'sharp' },
+    { letter: 'G', flavor: 'sharp' },
+    { letter: 'A', flavor: 'natural' },
+]));
