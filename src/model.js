8a21d7059159fdb4905e21a4bf3e109636d4d3a0
Mark Clifford <marklearninglanguage@gmail.com>
2022-03-25 12:00:41 -0700

    refactored Scaler.get scale to handle Majors obj

--- src/model.js
+++ src/model.js
@@ -69,24 +69,87 @@ var GMaj = [
     { letter: 'F', flavor: 'sharp' },
     { letter: 'G', flavor: 'natural' },
 ];
-//todo handle ABCDEFG major scales 
-//use if else to return majors given input, then make a major scale container object that can be called on for correct return
-Scalr.getScale = function (inp) {
-    if (inp === 'A') {
-        return AMaj;
-    } else if (inp === 'B') {
-        return BMaj
-    } else if (inp === 'C') {
-        return CMaj
-    } else if (inp === 'D') {
-        return DMaj
-    } else if (inp === 'E') {
-        return EMaj
-    } else if (inp === 'F') {
-        return FMaj
-    } else if (inp === 'G') {
-        return GMaj
+//todo handle ABCDEFG major scales ✔️✔️✔️
+//use if else to return majors given input ✔️✔️✔️
+//make a major scale container object that can be called on for correct return
 
-    }
+var majors = {
+    'A': [
+        { letter: 'A', flavor: 'natural' },
+        { letter: 'B', flavor: 'natural' },
+        { letter: 'C', flavor: 'sharp' },
+        { letter: 'D', flavor: 'natural' },
+        { letter: 'E', flavor: 'natural' },
+        { letter: 'F', flavor: 'sharp' },
+        { letter: 'G', flavor: 'sharp' },
+        { letter: 'A', flavor: 'natural' },
+    ],
+    'B': [
+        { letter: 'B', flavor: 'natural' },
+        { letter: 'C', flavor: 'sharp' },
+        { letter: 'D', flavor: 'sharp' },
+        { letter: 'E', flavor: 'natural' },
+        { letter: 'F', flavor: 'sharp' },
+        { letter: 'G', flavor: 'sharp' },
+        { letter: 'A', flavor: 'sharp' },
+        { letter: 'B', flavor: 'natural' },
+    ],
+    'C': [
+        { letter: 'C', flavor: 'natural' },
+        { letter: 'D', flavor: 'natural' },
+        { letter: 'E', flavor: 'natural' },
+        { letter: 'F', flavor: 'natural' },
+        { letter: 'G', flavor: 'natural' },
+        { letter: 'A', flavor: 'natural' },
+        { letter: 'B', flavor: 'natural' },
+        { letter: 'C', flavor: 'natural' },
+    ],
+    'D': [
+        { letter: 'D', flavor: 'natural' },
+        { letter: 'E', flavor: 'natural' },
+        { letter: 'F', flavor: 'sharp' },
+        { letter: 'G', flavor: 'natural' },
+        { letter: 'A', flavor: 'natural' },
+        { letter: 'B', flavor: 'natural' },
+        { letter: 'C', flavor: 'sharp' },
+        { letter: 'D', flavor: 'natural' },
+    ],
+    'E': [
+        { letter: 'E', flavor: 'natural' },
+        { letter: 'F', flavor: 'sharp' },
+        { letter: 'G', flavor: 'sharp' },
+        { letter: 'A', flavor: 'natural' },
+        { letter: 'B', flavor: 'natural' },
+        { letter: 'C', flavor: 'sharp' },
+        { letter: 'D', flavor: 'sharp' },
+        { letter: 'E', flavor: 'natural' },
+    ],
+    'F': [
+        { letter: 'F', flavor: 'natural' },
+        { letter: 'G', flavor: 'natural' },
+        { letter: 'A', flavor: 'natural' },
+        { letter: 'B', flavor: 'flat' },
+        { letter: 'C', flavor: 'natural' },
+        { letter: 'D', flavor: 'natural' },
+        { letter: 'E', flavor: 'natural' },
+        { letter: 'F', flavor: 'natural' },
+    ],
+    'G': [
+        { letter: 'G', flavor: 'natural' },
+        { letter: 'A', flavor: 'natural' },
+        { letter: 'B', flavor: 'natural' },
+        { letter: 'C', flavor: 'natural' },
+        { letter: 'D', flavor: 'natural' },
+        { letter: 'E', flavor: 'natural' },
+        { letter: 'F', flavor: 'sharp' },
+        { letter: 'G', flavor: 'natural' },
+    ]
+}
+Scalr.getScale = function (inp) {
+
+    return majors[inp];
+
 }
 
+
+//todo work on scale name output as part of scale object and Scaler.getScale
