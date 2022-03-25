855f51ba619c3fd789196d091a37440a4861d7ce
Mark Clifford <marklearninglanguage@gmail.com>
2022-03-24 22:04:15 -0700

    todo for morning

--- src/controller.js
+++ src/controller.js
@@ -29,7 +29,7 @@ function onReady() {
             text += note.letter;
             text += ' '
             text += note.flavor;
-            //todo work on basic text output for accidentals - if note.flavor  ===  flat {} ect...
+            //todo work on basic text output for accidentals - if note.flavor  ===  flat {note.flavor = b} ect...
 
             text += ' ';
             var $li = $('<li></li>') // li is a jquery object
