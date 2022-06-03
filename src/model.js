58e6178bce1f5989b4f6ec81b87fece285be4791
Mark Clifford <marklearninglanguage@gmail.com>
2022-06-03 15:53:52 -0700

    restructured Majors scales chromatically

--- src/model.js
+++ src/model.js
@@ -6,28 +6,11 @@ window.Scalr = {};
 // ♮♯♭
 
 (function () {
+    /**
+     * these scale centers are based on the tonalities found in J.S. Bach's Well Tempered Clavier as well as the modern circle of fiths (https://en.wikipedia.org/wiki/Circle_of_fifths) They're organized them chromatically starting with C (as Bach did in WTC), including relevant enharmonic equvalents.
+     */
     var major = {
         //natural majors
-        'A♮': [
-            { letter: 'A', flavor: 'natural' },
-            { letter: 'B', flavor: 'natural' },
-            { letter: 'C', flavor: 'sharp' },
-            { letter: 'D', flavor: 'natural' },
-            { letter: 'E', flavor: 'natural' },
-            { letter: 'F', flavor: 'sharp' },
-            { letter: 'G', flavor: 'sharp' },
-            { letter: 'A', flavor: 'natural' },
-        ],
-        'B♮': [
-            { letter: 'B', flavor: 'natural' },
-            { letter: 'C', flavor: 'sharp' },
-            { letter: 'D', flavor: 'sharp' },
-            { letter: 'E', flavor: 'natural' },
-            { letter: 'F', flavor: 'sharp' },
-            { letter: 'G', flavor: 'sharp' },
-            { letter: 'A', flavor: 'sharp' },
-            { letter: 'B', flavor: 'natural' },
-        ],
         'C♮': [
             { letter: 'C', flavor: 'natural' },
             { letter: 'D', flavor: 'natural' },
@@ -38,6 +21,26 @@ window.Scalr = {};
             { letter: 'B', flavor: 'natural' },
             { letter: 'C', flavor: 'natural' },
         ],
+        'C♯': [
+            { letter: 'C', flavor: 'sharp' },
+            { letter: 'D', flavor: 'sharp' },
+            { letter: 'E', flavor: 'sharp' },
+            { letter: 'F', flavor: 'sharp' },
+            { letter: 'G', flavor: 'sharp' },
+            { letter: 'A', flavor: 'sharp' },
+            { letter: 'B', flavor: 'sharp' },
+            { letter: 'C', flavor: 'sharp' },
+        ],
+        'D♭': [
+            { letter: 'D', flavor: 'flat' },
+            { letter: 'E', flavor: 'flat' },
+            { letter: 'F', flavor: 'natural' },
+            { letter: 'G', flavor: 'natural' },
+            { letter: 'A', flavor: 'flat' },
+            { letter: 'B', flavor: 'flat' },
+            { letter: 'C', flavor: 'natural' },
+            { letter: 'D', flavor: 'flat' },
+        ],
         'D♮': [
             { letter: 'D', flavor: 'natural' },
             { letter: 'E', flavor: 'natural' },
@@ -47,6 +50,16 @@ window.Scalr = {};
             { letter: 'B', flavor: 'natural' },
             { letter: 'C', flavor: 'sharp' },
             { letter: 'D', flavor: 'natural' },
+        ],
+        'E♭': [
+            { letter: 'E', flavor: 'flat' },
+            { letter: 'F', flavor: 'natural' },
+            { letter: 'G', flavor: 'natural' },
+            { letter: 'A', flavor: 'flat' },
+            { letter: 'B', flavor: 'flat' },
+            { letter: 'C', flavor: 'natural' },
+            { letter: 'D', flavor: 'natural' },
+            { letter: 'E', flavor: 'flat' },
         ],
         'E♮': [
             { letter: 'E', flavor: 'natural' },
@@ -68,17 +81,6 @@ window.Scalr = {};
             { letter: 'E', flavor: 'natural' },
             { letter: 'F', flavor: 'natural' },
         ],
-        'G♮': [
-            { letter: 'G', flavor: 'natural' },
-            { letter: 'A', flavor: 'natural' },
-            { letter: 'B', flavor: 'natural' },
-            { letter: 'C', flavor: 'natural' },
-            { letter: 'D', flavor: 'natural' },
-            { letter: 'E', flavor: 'natural' },
-            { letter: 'F', flavor: 'sharp' },
-            { letter: 'G', flavor: 'natural' },
-        ],
-        //sharp majors
         'F♯': [
             { letter: 'F', flavor: 'sharp' },
             { letter: 'G', flavor: 'sharp' },
@@ -89,36 +91,25 @@ window.Scalr = {};
             { letter: 'E', flavor: 'sharp' },
             { letter: 'F', flavor: 'sharp' },
         ],
-        'C♯': [
-            { letter: 'C', flavor: 'sharp' },
-            { letter: 'D', flavor: 'sharp' },
-            { letter: 'E', flavor: 'sharp' },
-            { letter: 'F', flavor: 'sharp' },
-            { letter: 'G', flavor: 'sharp' },
-            { letter: 'A', flavor: 'sharp' },
-            { letter: 'B', flavor: 'sharp' },
-            { letter: 'C', flavor: 'sharp' },
-        ],
-        //flat majors
-        'B♭': [
-            { letter: 'B', flavor: 'flat' },
-            { letter: 'C', flavor: 'natural' },
-            { letter: 'D', flavor: 'natural' },
-            { letter: 'E', flavor: 'flat' },
-            { letter: 'F', flavor: 'natural' },
-            { letter: 'G', flavor: 'natural' },
-            { letter: 'A', flavor: 'natural' },
-            { letter: 'B', flavor: 'flat' },
-        ],
-        'E♭': [
-            { letter: 'E', flavor: 'flat' },
-            { letter: 'F', flavor: 'natural' },
-            { letter: 'G', flavor: 'natural' },
+        'G♭': [
+            { letter: 'G', flavor: 'flat' },
             { letter: 'A', flavor: 'flat' },
             { letter: 'B', flavor: 'flat' },
+            { letter: 'C', flavor: 'flat' },
+            { letter: 'D', flavor: 'flat' },
+            { letter: 'E', flavor: 'flat' },
+            { letter: 'F', flavor: 'natural' },
+            { letter: 'G', flavor: 'flat' },
+        ],
+        'G♮': [
+            { letter: 'G', flavor: 'natural' },
+            { letter: 'A', flavor: 'natural' },
+            { letter: 'B', flavor: 'natural' },
             { letter: 'C', flavor: 'natural' },
             { letter: 'D', flavor: 'natural' },
-            { letter: 'E', flavor: 'flat' },
+            { letter: 'E', flavor: 'natural' },
+            { letter: 'F', flavor: 'sharp' },
+            { letter: 'G', flavor: 'natural' },
         ],
         'A♭': [
             { letter: 'A', flavor: 'flat' },
@@ -130,25 +121,36 @@ window.Scalr = {};
             { letter: 'G', flavor: 'natural' },
             { letter: 'A', flavor: 'flat' },
         ],
-        'D♭': [
-            { letter: 'D', flavor: 'flat' },
+
+        'A♮': [
+            { letter: 'A', flavor: 'natural' },
+            { letter: 'B', flavor: 'natural' },
+            { letter: 'C', flavor: 'sharp' },
+            { letter: 'D', flavor: 'natural' },
+            { letter: 'E', flavor: 'natural' },
+            { letter: 'F', flavor: 'sharp' },
+            { letter: 'G', flavor: 'sharp' },
+            { letter: 'A', flavor: 'natural' },
+        ],
+        'B♭': [
+            { letter: 'B', flavor: 'flat' },
+            { letter: 'C', flavor: 'natural' },
+            { letter: 'D', flavor: 'natural' },
             { letter: 'E', flavor: 'flat' },
             { letter: 'F', flavor: 'natural' },
             { letter: 'G', flavor: 'natural' },
-            { letter: 'A', flavor: 'flat' },
+            { letter: 'A', flavor: 'natural' },
             { letter: 'B', flavor: 'flat' },
-            { letter: 'C', flavor: 'natural' },
-            { letter: 'D', flavor: 'flat' },
         ],
-        'G♭': [
-            { letter: 'G', flavor: 'flat' },
-            { letter: 'A', flavor: 'flat' },
-            { letter: 'B', flavor: 'flat' },
-            { letter: 'C', flavor: 'flat' },
-            { letter: 'D', flavor: 'flat' },
-            { letter: 'E', flavor: 'flat' },
-            { letter: 'F', flavor: 'natural' },
-            { letter: 'G', flavor: 'flat' },
+        'B♮': [
+            { letter: 'B', flavor: 'natural' },
+            { letter: 'C', flavor: 'sharp' },
+            { letter: 'D', flavor: 'sharp' },
+            { letter: 'E', flavor: 'natural' },
+            { letter: 'F', flavor: 'sharp' },
+            { letter: 'G', flavor: 'sharp' },
+            { letter: 'A', flavor: 'sharp' },
+            { letter: 'B', flavor: 'natural' },
         ],
         'C♭': [
             { letter: 'C', flavor: 'flat' },
@@ -325,6 +327,7 @@ window.Scalr = {};
     // takes in a scale and returns a shallow copy of a scale plus all notes in reverse starting at the leading tone
 
     function descender(scale) {
+        scale = copyScale(scale);
         var full = []
         for (let i = 0; i < scale.length; i++) {
             full.push(scale[i]);
@@ -359,9 +362,23 @@ window.Scalr = {};
 
     //todo work on scale name output as part of scale object and Scaler.getScale based on tonality input
     //todo write test to check each note name and get all scales and prints any that are missing
+    //* from Bach WTC key centers - 
+    //* CM/m, C#M/m(four sharps), DM/m, EbM/D#m(6 sharps), EM/m, FM/m, F#M/m, GM/m, AbM/G#m(5 sharps), AM/m, BbM/m(5 flats), BM/m
+    //* i'll still include Eb minor, GbMajor, Cb Major, Db major
+    function scaleCheck(majorScaleObj, minorScaleObj) {
+        var scaleRef = {
+            major: 'A',
+            major: 'B',
+            major: 'C',
+        }
+    }
+
+
     // new approach might be needed for returning different scales. 
     // vexflow front end notation librtary
 
 
 })()
+
+
 
