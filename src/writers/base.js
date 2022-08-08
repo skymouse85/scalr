ff21824bcac284a16910737f5457fe7a8bbf5f4e
Mark Clifford <marklearninglanguage@gmail.com>
2022-08-08 15:29:52 -0700

    key sig

--- src/writers/base.js
+++ src/writers/base.js
@@ -1,3 +1,10 @@
+
+const OffsetSymbol = {
+    '0': '',
+    '1': '#',
+    '-1': 'b'
+}
+
 export default class BaseWriter {
     reload(scale, clef) {
         this.scale = scale;
@@ -6,7 +13,25 @@ export default class BaseWriter {
 
     }
 
+
+    // setupKeySig() {
+    //     this.keySig = this.scale.root.letter + getAccidental(this.scale.root);
+    //     //TODO refactor the switch to take this.keysig 
+    //     var tonality = this.scale.tonality
+    //     switch (tonality) {
+    //         case TONALITY.NATURAL_MINOR:
+    //         case TONALITY.HARMONIC_MINOR:
+    //         case TONALITY.MELODIC_MINOR:
+    //             this.keySig += 'm'
+    //             break;
+    //     }
+    // }
+
     render(target) {
         throw new Error('Not Implemented');
     }
 }
+
+// function getAccidental(note) {
+//     return OffsetSymbol[note.offset]
+// }
